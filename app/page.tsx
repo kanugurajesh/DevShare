1192b6d6a403b45e0c99f442637becb1ad6304db
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-18 08:04:44 +0530

    Adding a new favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
-    
-    </>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto quos, aut tenetur cumque a enim sed quaerat nam, libero eaque quam numquam eligendi excepturi! Officia odit porro praesentium fugit velit sit consectetur?    
+      </div>
     )
 }
