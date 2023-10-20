b384e39f3ba840d0bafa23edf25f2f917602d170
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-20 16:14:23 +0530

    My Dev Blog

--- components/categoryList/CategoryList.tsx
+++ components/categoryList/CategoryList.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import styles from './categoryList.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const CategoryList = () => {
   return (
-    <div>CategoryList</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link href={"/blog?cat=style"} className={`${styles.category} ${styles.style}`}>
+          <Image src={"/webapp.png"} alt='' width={32} height={32} className={styles.image}/>
+          Web
+        </Link>
+        <Link href={"/blog"} className={`${styles.category} ${styles.style}`}>
+          <Image src={"/mobileapp.png"} alt='' width={32} height={32} className={styles.image}/>
+          Mobile
+        </Link>
+        <Link href={"/blog"} className={`${styles.category} ${styles.style}`}>
+          <Image src={"/machinelearning.png"} alt='' width={32} height={32} className={styles.image}/>
+          ML
+        </Link>
+        <Link href={"/blog"} className={`${styles.category} ${styles.style}`}>
+          <Image src={"/dsa.png"} alt='' width={32} height={32} className={styles.image}/>
+          DSA
+        </Link>
+        <Link href={"/blog"} className={`${styles.category} ${styles.style}`}>
+          <Image src={"/career.png"} alt='' width={32} height={32} className={styles.image}/>
+          Career
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -5,15 +5,15 @@ import Image from 'next/image'
 const Featured = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}><b>Hey, Rajesh here!</b> Discover my stories and creative ideas</h1>
+      <h1 className={styles.title}><b>👋 Devs,</b> Discover developer stories and ideas here!</h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
-          <Image src={"/p1.jpeg"} alt='' fill className={styles.image}/>
+          <Image src={"/passion.jpg"} alt='' fill className={styles.image}/>
         </div>
         <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>Hello Welcome to tech boys share your blogs here</h1>
+          <h1 className={styles.postTitle}>👋 welcome to <b>DevShare</b> share your blogs here</h1>
           <p className={styles.postDesc}>
-            Hi Tech Boys is my initiative which enables developers to share their blogs and articles with the community.
+          👋 everyone <b>DevShare</b> is an initiative which enables developers to share their blogs and articles with the community.
           </p>
           <button className={styles.button}>Read More</button>
         </div>
