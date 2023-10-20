b7314c789ff4d575236bf059b87e57f83d088e34
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-20 09:20:55 +0530

    Adding responsiveness and adding colors to toggle button

--- components/themeToggle/themeToggle.tsx
+++ components/themeToggle/themeToggle.tsx
@@ -10,9 +10,9 @@ const ThemeToggle = () => {
     // importing the theme from the context
     const {toggle,theme}:any = useContext(ThemeContext);
 
-    return <div className={styles.container} onClick={toggle}>
+    return <div className={styles.container} onClick={toggle} style={theme === "dark" ? {backgroundColor: "white"}: { backgroundColor:"#0f172a" }}>
         <Image src={"/moon.png"} alt='' width={14} height={14}/>
-        <div className={styles.ball} style={theme == "dark" ? {left:1.5,background:"white"}:{right:1.5,background:"#0f172a"}}></div>
+        <div className={styles.ball} style={theme == "dark" ? {left:1.5,background:"#0f172a"}:{right:1.5,background:"white"}}></div>
         <Image src={"/sun.png"} alt='' width={14} height={14}/>
     </div>
 
