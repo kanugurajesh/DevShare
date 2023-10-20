a66f8d1cc34dd72c53822f0b470646d7dc676d4f
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-20 08:42:41 +0530

    Making the ui responsive

--- components/themeToggle/themeToggle.tsx
+++ components/themeToggle/themeToggle.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import styles from './themeToggle.module.css'
-import Image from 'next/image'
-import { useContext } from "react"
-import { ThemeContext } from '../context/ThemeContext'
+import styles from './themeToggle.module.css';
+import Image from 'next/image';
+import { useContext } from "react";
+import { ThemeContext } from '../context/ThemeContext';
 
 const ThemeToggle = () => {
 
@@ -12,7 +12,7 @@ const ThemeToggle = () => {
 
     return <div className={styles.container} onClick={toggle}>
         <Image src={"/moon.png"} alt='' width={14} height={14}/>
-        <div className={styles.ball} style={theme == "dark" ? {left:1,background:"white"}:{right:1,background:"#0f172a"}}></div>
+        <div className={styles.ball} style={theme == "dark" ? {left:1.5,background:"white"}:{right:1.5,background:"#0f172a"}}></div>
         <Image src={"/sun.png"} alt='' width={14} height={14}/>
     </div>
 
