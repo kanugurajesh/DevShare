5c85735fd950123765693964909a4ed66d9c3da8
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-20 15:00:09 +0530

    Adding the first post

--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import styles from './featured.module.css'
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}><b>Hey, Rajesh here!</b> Discover my stories and creative ideas</h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src={"/p1.jpeg"} alt='' fill/>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Hello Welcome to tech boys share your blogs here</h1>
+          <p className={styles.postDesc}>
+            Hi Tech Boys is my initiative which enables developers to share their blogs and articles with the community.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
