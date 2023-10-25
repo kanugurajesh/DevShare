40778da71dd2913301d8ea8d3913c630a64211ac
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-26 00:41:41 +0530

    Making UI changes

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -6,94 +6,153 @@ import Image from 'next/image'
 const Menu = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.subtitle}>What&apos;s hot</h2>
-      <h1 className={styles.title}>Most Popular</h1>
-      <div className={styles.items}>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>
-                John Doe
-              </span>
-              <span className={styles.date}>- 10.03.2023</span>
+      <div className={styles.entity}>
+        <h2 className={styles.subtitle}>What&apos;s hot</h2>
+        <h1 className={styles.title}>Most Popular</h1>
+        <div className={styles.items}>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
             </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>
-                John Doe
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
               </span>
-              <span className={styles.date}>- 10.03.2023</span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>
-                John Doe
+          </Link>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
               </span>
-              <span className={styles.date}>- 10.03.2023</span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>
-                John Doe
+          </Link>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
               </span>
-              <span className={styles.date}>- 10.03.2023</span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>
-                John Doe
+          </Link>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
               </span>
-              <span className={styles.date}>- 10.03.2023</span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.entity}>
+        <h2 className={styles.subtitle}>Choosen by the editor</h2>
+        <h1 className={styles.title}>Editor Pick</h1>
+        <div className={styles.items}>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
+              </span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
+            </div>
+          </Link>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
+              </span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
+            </div>
+          </Link>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
+              </span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
+            </div>
+          </Link>
+          <Link href="/" className={styles.item}>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+              <span className={`${styles.category} ${styles.travel}`}>
+                Travel
+              </span>
+              <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+              <div className={styles.detail}>
+                <span className={styles.username}>
+                  John Doe
+                </span>
+                <span className={styles.date}>- 10.03.2023</span>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   )
