747d2f89bd2a4a10326e746021fe44594181505e
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-25 23:20:22 +0530

    Adding posts

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import styles from './menu.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What's hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="1" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, molestiae.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>
+                John Doe
+              </span>
+              <span className={styles.date}>- 10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
   )
 }
 
---
+++ components/card/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './card.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt='' fill className={styles.image}/>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href='/'>
+          <h1 className={styles.h1}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, optio?</h1>
+        </Link>
+        <p className={styles.desc}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti ea temporibus sed officia. Ut magni magnam aut amet nemo alias aperiam molestiae commodi cum enim unde exercitationem minus pariatur eos repudiandae illo quis vero placeat mollitia, velit fuga ex consectetur.</p>
+        <Link href='/' className={styles.link}>Read More</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Card
